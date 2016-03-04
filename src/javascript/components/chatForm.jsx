7c6c1b986e2ec74fd8d5c06950aa1d7e9fee6a67
碎碎酱 <yinxin630@gmail.com>
2016-03-04 18:27:39 +0800

    feat(chatform): add message form scroll

--- src/javascript/components/chatForm.jsx
+++ src/javascript/components/chatForm.jsx
@@ -3,14 +3,26 @@
 const React = require('react');
 
 export default class ChatForm extends React.Component {
+    static defaultProps = {
+        paddingBottom: 10,
+    }
+    
+    componentDidUpdate () {
+        let form = this.refs.chatform.getDOMNode()
+        let maxHeight = form.scrollHeight - form.clientHeight;
+        if (form.scrollTop >= maxHeight - this.props.paddingBottom - (form.lastElementChild.offsetHeight) || 0) {
+            form.scrollTop = maxHeight;
+        }
+    }
+    
     render () {
         return (
             <div style={{
                 flex: 1,
                 borderBottom: '1px solid #e0e0e0',
                 overflow: 'auto',
-                paddingBottom: 10,
-            }}>
+                paddingBottom: this.props.paddingBottom,
+            }} ref="chatform" id="aa">
                 { this.props.children }
             </div>
         );
