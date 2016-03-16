9a2b957daf4a67d20929ffa4d8f20b714a919f98
碎碎酱 <yinxin630@gmail.com>
2016-03-16 12:10:04 +0800

    refactor(about): modify about text

--- src/javascript/pages/about.jsx
+++ src/javascript/pages/about.jsx
@@ -11,7 +11,8 @@ export default class Source extends React.Component {
                 paddingTop: 100,
             }}>
                 <label>版本：v1.3.0</label> <br/>
-                <label></label> <br/>
+                <label>新增功能: 发图片, 发URL</label> <br/>
+                <label>BUG修复: 头像类型,大小限制, 消息长度限制, 头像放到七牛存储, 几个表情错误</label> <br/>
                 <br/>
                 <img src="../../images/star.png"/> <br/>
                 <label>(●'◡'●)ﾉ♥觉得还不错的话欢迎点赞鼓励，↓这俩货就是源码</label> <br/>
