1f1194bdd4bab1f34fd4f68f9529793865d474e5
碎碎酱 <yinxin630@gmail.com>
2016-03-17 11:49:51 +0800

    refactor(message): use avatar instead this.props.avatar

--- src/javascript/components/message.jsx
+++ src/javascript/components/message.jsx
@@ -63,7 +63,7 @@ export default class Message extends React.Component {
                 marginTop: 10,
                 flexDirection: align !== 'right' || 'row-reverse', 
             }}>
-                    <Image src={ this.props.avatar }
+                    <Image src={ avatar }
                     width={40} height={40} circle
                 />
                 <div style={{
