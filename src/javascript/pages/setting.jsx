cef07beb2b197e1d1b2056d5f47d8f74d786ad81
碎碎酱 <yinxin630@gmail.com>
2016-03-21 14:22:15 +0800

    refactor(setting): use antd instread amazeui

--- src/javascript/pages/setting.jsx
+++ src/javascript/pages/setting.jsx
@@ -1,48 +1,55 @@
 'use strict'
 
 const React = require('react');
-import { Form, fieldset, Input, Button } from 'amazeui-react';
+import { Form, Input, Button, message } from 'antd';
+const FormItem = Form.Item;
 
 export default class Setting extends React.Component {
     render () {
         const { handleSetting } = this.props;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 15 },
+        };
         
         return (
             <div style={{
                 flex: 1,
             }}>
                 <div style={{
-                    width: 350,
+                    width: 450,
                     margin: '100px auto',
                     textAlign: 'center',
                 }}>
-                    <Form>
-                        <span style={{
-                            color: 'red',
-                        }} ref="info">
-                        </span>
-                        <fieldset className="am-form-set">
-                            <Input type="text" placeholder="昵称" icon="user" ref="nickname" maxLength={8}/>
-                            <Input type="file" label="头像" id="avatar" accept="image/*"/>
-                            <Button amStyle="primary" block onClick={ e => {
-                                let avatar = document.querySelector('#avatar').files[0];
-                                if (avatar && avatar.size > 2048000) {
-                                    this.refs.info.innerText = '文件过大, 请选择小于2MB的文件';
-                                    return;
-                                }
-                                
-                                let nickname = this.refs.nickname.getValue();
-                                if (!avatar) {
-                                    return handleSetting(nickname, '');
-                                }
-                                let reader = new FileReader();
-                                reader.onloadend = function() {
-                                    return handleSetting(nickname, this.result);
-                                };
-                                reader.readAsDataURL(avatar);
-                            } }>保存</Button>
-                        </fieldset>
+                    <Form horizontal>
+                        <FormItem
+                            {...formItemLayout}
+                            label="昵称：">
+                            <Input type="text" ref="nickname" placeholder="新昵称" maxLength={12}/>
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="头像：">
+                            <Input type="file" ref="avatar" accept="image/*"/>
+                        </FormItem>
                     </Form>
+                    <Button amStyle="primary" block onClick={ e => {
+                        let avatar = this.refs.avatar.refs.input.files[0];
+                        if (avatar && avatar.size > 2048000) {
+                            this.refs.info.innerText = '文件过大, 请选择小于2MB的文件';
+                            return;
+                        }
+                        
+                        let nickname = this.refs.nickname.refs.input.value;
+                        if (!avatar) {
+                            return handleSetting(nickname, '');
+                        }
+                        let reader = new FileReader();
+                        reader.onloadend = function() {
+                            return handleSetting(nickname, this.result);
+                        };
+                        reader.readAsDataURL(avatar);
+                    } }>保存</Button>
                 </div>
             </div>
         );
