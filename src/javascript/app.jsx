fed8b4eda621acb9b7e9f5eed5b3f306b8dc93c0
碎碎酱 <yinxin630@gmail.com>
2016-03-22 13:50:47 +0800

    fix(app): fix send empty message bug

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -117,7 +117,7 @@ export default class App extends React.Component {
     }
     
     handleSend (content, type, linkman) {
-        if (!content || content === '') {
+        if (!content || content.text === '') {
             return;
         }
         
