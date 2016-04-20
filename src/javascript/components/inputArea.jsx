24fcc36a2d18ecbce336ea559a8965c57b78e577
碎碎酱 <yinxin630@gmail.com>
2016-04-20 15:21:51 +0800

    fix(inputarea): fix import error

--- src/javascript/components/inputArea.jsx
+++ src/javascript/components/inputArea.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import Radium from 'Radium';
+import Radium from 'radium';
 import ExpressionForm from './expressionForm.jsx';
 
 class InputArea extends React.Component {
