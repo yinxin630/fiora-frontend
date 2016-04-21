29fc9d27c7b6049b1588f5242b5f984be3613c8d
碎碎酱 <yinxin630@gmail.com>
2016-04-21 12:37:32 +0800

    feat(about): update about page

--- src/javascript/pages/about.jsx
+++ src/javascript/pages/about.jsx
@@ -1,6 +1,14 @@
-'use strict'
+'use strict';
 
 const React = require('react');
+const functions = [
+    '支持游客, 支持注册/登录',
+    '支持头像修改/保存',
+    '支持文字/表情/图片消息',
+    '支持群聊/私聊',
+    '支持登录用户消息存储',
+    '支持新消息桌面通知'
+];
 
 export default class Source extends React.Component {
     render () {
@@ -8,16 +16,23 @@ export default class Source extends React.Component {
             <div style={{
                 flex: 1,
                 textAlign: 'center',
-                paddingTop: 100,
+                paddingTop: 100
             }}>
-                <label>版本：v1.4.1</label> <br/>
-                <label>新增功能: 查看图片原图, 游客提示, 游客消息标注, 注册登录等功能的错误提示优化</label> <br/>
-                <label>BUG修复: firefox,safari发图高度为0, 游客不能发消息, 历史消息记录无序, 用户发消息接口bug</label> <br/>
+                <label style={{fontSize: '1.5rem'}}>版本: v1.5.0</label> <br/>
+                <label style={{fontSize: '1.3rem'}}>新增功能: 私聊, 滚动条样式, 通过图片链接直接发图, 关闭应用后登录状态</label> <br/>
+                <label style={{fontSize: '1.3rem'}}>BUG修复: 修改消息内容不正确的正则规则, 优化启用消息通知场景</label> <br/>
+                <br/>
+                <label style={{fontSize: '1.5rem'}}>聊天室功能</label>
+                <ul>
+                {
+                    functions.map(x => <li style={{fontSize: '1.3rem', display: 'list-item', listStyleType: 'disc'}}>{ x }</li>)
+                }
+                </ul>
                 <br/>
                 <img src="images/star.png"/> <br/>
-                <label>(●'◡'●)ﾉ♥觉得还不错的话欢迎点赞鼓励，↓这俩货就是源码</label> <br/>
-                <label>前端：</label><a href="https://github.com/yinxin630/fiora-frontend" target="_blank">fiora-frontend</a> <br/>
-                <label>后端：</label><a href="https://github.com/yinxin630/fiora-backend" target="_blank">fiora-backend</a> <br/>
+                <label style={{fontSize: '1.5rem'}}>(●'◡'●)ﾉ♥觉得还不错的话请帮忙在Github上点个赞，↓这俩货就是源码</label> <br/>
+                <label style={{fontSize: '1.5rem'}}>前端：</label><a  style={{fontSize: '1.5rem'}} href="https://github.com/yinxin630/fiora-frontend" target="_blank">fiora-frontend</a> <br/>
+                <label style={{fontSize: '1.5rem'}}>后端：</label><a  style={{fontSize: '1.5rem'}} href="https://github.com/yinxin630/fiora-backend" target="_blank">fiora-backend</a> <br/>
             </div>
         );
     }
